38ef5a936cfd00196f9f229ca5fec2dac3597b3b
Edris9 <Edriskohestani1010@gmail.com>
2026-02-21 03:22:31 +0100

    fixade null i klassen som skulle vara tom men jag sa fel

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,9 @@
-﻿namespace Game__Tournament_API.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+﻿namespace Game__Tournament_API.Models;
 
-    }
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
